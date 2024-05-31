b01c3072ef810a6c2efdbbc384146145d21cd897
GuiNegr <GuiNeg.git@outlook.com>
2024-05-31 16:13:31 -0300

    Upando O ultimo crud

--- ConSult/src/Controller/MedicoController.java
+++ ConSult/src/Controller/MedicoController.java
@@ -5,7 +5,10 @@ import Model.Medico;
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedicoController {
 
@@ -19,11 +22,56 @@ public class MedicoController {
             preparedStatement.setString(2,medico.getNomeMedico());
             preparedStatement.execute();
             JOptionPane.showMessageDialog(null,"medico criado");
-
         }catch (SQLException e){
             JOptionPane.showMessageDialog(null,"ERROR CREATE MEDICO: "+e.getMessage());
         }
+    }
 
+    public void deleteMedico(Medico medico){
+        String sql = "Delete from medicos where crmMedico = ?";
+        try {
+            Connection connection = new ConexaoDao().conecta();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1,medico.getCrmMedico());
+            preparedStatement.execute();
+        }
+        catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"ERROR DELETEMEDICO: "+e.getMessage());
+        }
+    }
+
+    public List<Medico> readMedico(){
+        String sql = "Select * from medicos";
+        List<Medico> medicos = new ArrayList<>();
+        try {
+            Connection connection = new ConexaoDao().conecta();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                String nome = resultSet.getString("nomeMedico");
+                int crm = resultSet.getInt("crmMedico");
+                Medico medico = new Medico(nome,crm);
+                medicos.add(medico);
+            }
+            return medicos;
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"ERROR READ MEDICOS: "+e.getMessage());
+            return null;
+        }
+    }
+
+    public void updateMedic(Medico medico){
+        String sql = "UPDATE MEDICOS SET NOMEMEDICO = ? WHERE CRMMEDICO = ?";
+
+        try {
+            Connection connection = new ConexaoDao().conecta();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,medico.getNomeMedico());
+            preparedStatement.setInt(2,medico.getCrmMedico());
+            preparedStatement.execute();
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"ERROR UPDATE MEDIC: "+e.getMessage());
+        }
 
     }
 }
--- ConSult/src/Main.java
+++ ConSult/src/Main.java
@@ -1,7 +1,16 @@
 import Controller.ConsultaController;
+import Controller.MedicoController;
+import Model.Consulta;
+import Model.Medico;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new ConsultaController().readConsulta("1234562891"));
+        Medico medico = new Medico("DR Guilherme secola rei do java SECO",300300);
+        MedicoController mdc = new MedicoController();
+        mdc.updateMedic(medico);
     }
 }
