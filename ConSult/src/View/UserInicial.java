55dd153032dd2fc0331de6e70aaaaaaa8e0ba7a2
GuiNegr <GuiNeg.git@outlook.com>
2024-06-01 10:51:07 -0300

    Colocando a view

---
+++ ConSult/src/View/UserInicial.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.*;
+
+public class UserInicial extends JFrame{
+    private JLabel consult;
+    private  JButton rdPac;
+    private JButton rdMedc;
+    private JButton crtPac;
+    private JButton crtConsu;
+    private JButton crtMedc;
+
+    public UserInicial(){
+
+    }
+}
